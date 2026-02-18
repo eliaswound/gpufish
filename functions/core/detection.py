6b03950b408404f1d0c9ee509e91792773ffcc3d
eliaswound <eliaswound@gmail.com>
2026-02-18 16:21:41 -0600

    Update detection.py

--- functions/core/detection.py
+++ functions/core/detection.py
@@ -1,15 +1,18 @@
-
+from .filter import log_filter
+from .filter import local_maximum_filter
+from .conventional_threshold import _get_candidate_thresholds
+from .conventional_threshold import _get_spot_threshold
+from .conventional_threshold import spots_thresholding
 def detect_spots(
         image,
         threshold=None,
-        remove_duplicate=True,
         return_threshold=False,
         voxel_size=None,
         spot_radius=None,
         log_kernel_size=None,
         minimum_distance=None):
 """ detect spots in image using gpu if available, otherwise use cpu
-Parameters:
+    Parameters:
     image:single instance of image to detect spots in, input should be np.arrary or torch.tensor
     threshold: threshold for detecting spots, if not provided, a heuristic threshold will be used
     remove_duplicate: remove duplicate spots, default is True
@@ -19,16 +22,38 @@ Parameters:
     log_kernel_size: size of the log kernel, default is None
     minimum_distance: minimum distance between spots, default is None
 """
-    # Sanity check of image size 
-    n = len(image)
-    print("image size: ", image.shape)
-
-    # apply LoG filter and find local maximum
-    image_filtered = []
     pixel_values = []
     masks = []
-    for image in images:
-        # filter image
-        image_filtered = stack.log_filter(image, log_kernel_size)
-        images_filtered.append(image_filtered)
-        # apply LoG filter and find local maximum
+    image_filtered = []
+    print("detecting spots in image")
+    for item in image:
+        # filter image using log filter
+        image_filtered = stack.log_filter(item, log_kernel_size)
+        image_filtered.append(image_filtered)
+        # get pixels value
+        pixel_values += list(image_filtered.ravel())
+        # find local maximum using local maximum filter
+        mask_local_max = local_maximum_filter(image_filtered, minimum_distance)
+        masks.append(mask_local_max)
+    if threshold is None:
+        print("no threshold provided, finding threshold now")
+         thresholds = _get_candidate_thresholds(pixel_values)
+         threshold = _get_spot_threshold(thresholds)
+         print("threshold: ", threshold)
+    else: 
+        print(" using provided threshold: ", threshold)
+    all_spots = []
+    for i in range(n):
+        # get image and mask
+        image_filtered = images_filtered[i]
+        mask_local_max = masks[i]
+ # detect spots using conventional thresholding
+        spots, _ = spots_thresholding(
+            image_filtered, mask_local_max, threshold)
+        all_spots.append(spots)
+
+    # return threshold or not
+    if return_threshold:
+        return all_spots, threshold
+    else:
+        return all_spots
